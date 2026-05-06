4e7903421186badadf7f1b4da344f2619b0b7085
karinash20 <cuteblackcb@gmail.com>
2026-05-06 13:34:45 +0500

    ArticleWidget

--- lib/main.dart
+++ lib/main.dart
@@ -23,36 +23,65 @@ class ArticleModel {
   Future<Summary> getRandomArticle() async {
     final uri = Uri.https(
       'en.wikipedia.com',
-      'api/rest_v1/page/random/summary'
+      'api/rest_v1/page/random/summary',
     );
     final response = await get(uri);
-    if (response.statusCode != 200){
+    if (response.statusCode != 200) {
       throw HttpException("Failed to update resource");
-    } 
-    return Summary.fromJson(
-      json.decode(response.body) as Map <String,Object?>);
+    }
+    return Summary.fromJson(json.decode(response.body) as Map<String, Object?>);
   }
 }
 
-class ArticleViewModel extends ChangeNotifier{
+class ArticleViewModel extends ChangeNotifier {
   final ArticleModel model;
   Summary? summary;
   Exception? error;
   bool isLoading = false;
-  ArticleViewModel (this.model){
+  ArticleViewModel(this.model) {
     fetchArticle();
   }
   void fetchArticle() async {
     isLoading = true;
     notifyListeners();
-    try{
+    try {
       summary = await model.getRandomArticle();
       error = null;
-    } on HttpException catch (e){
+    } on HttpException catch (e) {
       summary = null;
       error = e;
     }
     isLoading = false;
     notifyListeners();
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  final Summary summary;
+  const ArticleWidget({super.key, required this.summary});
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 10,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!source),
+        ];
+        Text(
+          summary.titles.normalized,
+          overflow: Overflow.ellipsis,
+          style: Theme.of(context).textTheme.displaySmall
+        ),
+        if (summary description != null),
+        Text(
+          summary.description!,
+          overflow: Overflow.ellipsis,
+          style: Theme.of(context).textTheme.displaySmall
+        ),
+        Text(
+          summary.extract
+        )
+      ),
+    );
   }
 }
